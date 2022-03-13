6e01d3ee4e15423ee7b8ce20db5a5c78be169f2e
hanmok <dmammmm@naver.com>
2022-03-14 07:00:24 +0900

    change localized Structs into Enum

--- Utils/ColorExt.swift
+++ Utils/ColorExt.swift
@@ -31,16 +31,6 @@ extension Color {
       (Int(r * 255) << 24)
   }
     
-//    static var mainColor = Color(.sRGB, red: 140/255, green: 180/255, blue: 140/255)
-    
-    // Those two are mainColors
-//    static var subColor = Color(.sRGB, red: 100/255, green: 200/255, blue: 100/255)
-    
-//    static var subColorDark = Color(.sRGB, red: 50/255, green: 150/255, blue: 50/255)
-    
-    // subColor
-//    static var subColor2 = Color(.sRGB, red: 130/255, green: 230/255, blue: 130/255)
-
     static var subColorDark2 = Color(.sRGB, red: 80/255, green: 180/255, blue: 80/255)
     
     // second subColor
@@ -49,7 +39,6 @@ extension Color {
     
     // ivory Colors
     
-//    static let ivory = Color(.sRGB, red: 214/255, green: 207/255, blue: 181/255)
     static let ivory = Color(red: 214, green: 207, blue: 181)
     
     // MAIN COLOR
@@ -59,48 +48,6 @@ extension Color {
     static let darkCream = Color(red: 144, green: 140, blue: 111)
     static let ecru = Color(red: 237, green: 223, blue: 200)
     
-    
-//    static let pastelColors: [Color] = [
-//        Color(red: 235, green: 165, blue: 138),
-//        Color(red: 242, green: 196, blue: 181),
-//        Color(red: 238, green: 191, blue: 202),
-//        Color(red: 245, green: 203, blue: 141),
-//        Color(red: 251, green: 245, blue: 173),
-//        Color(red: 146, green: 199, blue: 160),
-//        Color(red: 172, green: 211, blue: 198),
-//        Color(red: 207, green: 228, blue: 191),
-//        Color(red: 221, green: 240, blue: 251),
-//        Color(red: 197, green: 222, blue: 243),
-//        Color(red: 175, green: 194, blue: 225),
-//        Color(red: 229, green: 209, blue: 224),
-////        someIvorys
-//        Color(red: 248, green: 250, blue: 230),
-//        Color(red: 254, green: 253, blue: 246),
-//        Color(red: 246, green: 244, blue: 232)
-//        // original
-////        Color(red: 198, green: 184, blue: 215),
-////        Color(red: 221, green: 221, blue: 237),
-////        Color(red: 226, green: 195, blue: 199)
-//    ]
-    
-    // cream Color Tests, lighter
-//    static let pastelColors: [Color] = [
-//        Color(red: 244, green: 240, blue: 211),
-//        Color(red: 245, green: 241, blue: 212),
-//        Color(red: 246, green: 242, blue: 213),
-//        Color(red: 247, green: 243, blue: 214),
-//        Color(red: 248, green: 244, blue: 215),
-//        Color(red: 249, green: 245, blue: 216),
-//        Color(red: 250, green: 246, blue: 217),
-//        Color(red: 251, green: 247, blue: 218),
-//        Color(red: 252, green: 248, blue: 219),
-//        Color(red: 253, green: 249, blue: 220),
-//        Color(red: 254, green: 250, blue: 221),
-//        Color(red: 255, green: 251, blue: 222),
-//        Color(red: 244, green: 252, blue: 223),
-//        Color(red: 244, green: 253, blue: 224),
-//        Color(red: 244, green: 254, blue: 225)
-//    ]
     
     static let pastelColors: [Color] = [
         Color(red: 244, green: 240, blue: 211),
